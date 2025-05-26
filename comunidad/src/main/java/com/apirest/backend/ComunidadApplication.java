a6fe494a1663ab764f1a358eae0edfe553d35d2d
EstebanVSa <esteban.vargas@uao.edu.co>
2025-05-26 13:19:36 -0500

    fix Arreglado archivo haciendo conflicto

--- comunidad/src/main/java/com/apirest/backend/ComunidadApplication.java
+++ comunidad/src/main/java/com/apirest/backend/ComunidadApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class ComunidadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(ComunidadApplication.class, args);
 	}
 
 }
