024a0acdf11a1f72312162a4acb68ff70fb2cc98
EstebanVSa <esteban.vargas@uao.edu.co>
2025-05-26 13:41:56 -0500

    Por favor funciona Si no funciona voy a cometer Persona 3

--- comunidad/src/main/java/com/apirest/backend/ComunidadApplication.java
+++ comunidad/src/main/java/com/apirest/backend/ComunidadApplication.java
@@ -1,14 +1,12 @@
 package com.apirest.backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComunidadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComunidadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ComunidadApplication.class, args);
+    }
 }
